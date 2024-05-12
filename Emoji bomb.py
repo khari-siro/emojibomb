6caf9bef913e9c7cf78b1103daf41b29fe54907d
Mercy Siro <kharisiro@gmail.com>
2024-05-12 05:37:45 -0700

    Add files via upload

---
+++ Emoji bomb.py
@@ -0,0 +1,124 @@
+from tkinter import*
+import tkinter
+
+
+top=tkinter.Tk()
+top.geometry('800x600+0+0')
+top.title('Emoji bomb')
+top.config(bg='green')
+top.resizable(False,False)
+def emoji1():
+    top=tkinter.Tk()
+    top.geometry('800x600+0+0')
+    top.title('Emoji bomb')
+    top.config(bg='green')
+    top.resizable(False,False)
+    r=Label(top,text="😁",bg='gold',font='Arial 120 bold').place(x=260,y=50)
+    u=Label(top,text="The Grin emoji",font='Arial 20 bold').place(x=250,y=250)
+    k=Label(top,text="I just got some news... I would have just smiled but it requires a greater reaction",font='Times 16 bold').place(x=20,y=400)
+    a=Label(top,text="😁 😁 😁 😁 😁",font='Arial 40 bold',bg="gold").place(x=150,y=500)
+    
+def emoji2():
+    top=tkinter.Tk()
+    top.geometry('800x600+0+0')
+    top.title('Emoji bomb')
+    top.config(bg='green')
+    r=Label(top,text="🤣",bg='lightgreen',font='Arial 120 bold').place(x=260,y=50)
+    u=Label(top,text="The laugh emoji",font='Arial 20 bold').place(x=250,y=250)
+    k=Label(top,text="That meme ... It's so funny... I just can't",font='Times 16 bold').place(x=200,y=400)
+    a=Label(top,text="🤣🤣🤣🤣🤣",font='Arial 40 bold',bg="lightgreen").place(x=200,y=500)
+
+def emoji3():
+    top=tkinter.Tk()
+    top.geometry('800x600+0+0')
+    top.title('Emoji bomb')
+    top.config(bg='green')
+    top.resizable(False,False)
+    r=Label(top,text="🥰",bg='red',font='Arial 120 bold').place(x=260,y=50)
+    u=Label(top,text="The loveee emoji",font='Arial 20 bold').place(x=240,y=250)
+    k=Label(top,text="This love ... woah!!! I can feel it all over",font='Times 16 bold').place(x=150,y=400)
+    a=Label(top,text="🥰 🥰 🥰 🥰 🥰",font='Arial 40 bold',bg="red").place(x=150,y=500)
+
+def emoji4():
+    top=tkinter.Tk()
+    top.geometry('800x600+0+0')
+    top.title('Emoji bomb')
+    top.config(bg='green')
+    top.resizable(False,False)
+    r=Label(top,text="😎",bg='gray',font='Arial 120 bold').place(x=260,y=50)
+    u=Label(top,text="The COOL emoji",font='Arial 20 bold').place(x=240,y=250)
+    k=Label(top,text="Am I a parking ticket? Cause I have fine written all over me",font='Times 16 bold').place(x=150,y=400)
+    a=Label(top,text="😎 😎 😎 😎 😎",font='Arial 40 bold',bg="gray").place(x=200,y=500)
+
+def emoji5():
+    top=tkinter.Tk()
+    top.geometry('800x600+0+0')
+    top.title('Emoji bomb')
+    top.config(bg='green')
+    top.resizable(False,False)
+    r=Label(top,text="😊",bg='orange',font='Arial 120 bold').place(x=260,y=50)
+    u=Label(top,text="The SCHMILE emoji",font='Arial 20 bold').place(x=230,y=250)
+    k=Label(top,text="I'm smiling and I look really cute doing it",font='Times 16 bold').place(x=180,y=400)
+    a=Label(top,text="😊😊😊😊😊",font='Arial 40 bold',bg="orange").place(x=200,y=500)
+
+def emoji6():
+    top=tkinter.Tk()
+    top.geometry('800x600+0+0')
+    top.title('Emoji bomb')
+    top.config(bg='green')
+    top.resizable(False,False)
+    r=Label(top,text="🤩",bg='purple',font='Arial 120 bold').place(x=260,y=50)
+    u=Label(top,text="The ROCKSTAR emoji",font='Arial 20 bold').place(x=200,y=250)
+    k=Label(top,text="Imma Imma Imma ... Starboy!!!",font='Times 16 bold').place(x=220,y=400)
+    a=Label(top,text="🤩🤩🤩🤩🤩",font='Arial 40 bold',bg="purple").place(x=200,y=500)
+
+def emoji7():
+    top=tkinter.Tk()
+    top.geometry('800x600+0+0')
+    top.title('Emoji bomb')
+    top.config(bg='green')
+    top.resizable(False,False)
+    r=Label(top,text="🤗",bg='pink',font='Arial 120 bold').place(x=260,y=50)
+    u=Label(top,text="The HANDS-OUT emoji",font='Arial 20 bold').place(x=200,y=250)
+    k=Label(top,text="I'm here when you need anything. I'll always be there for you",font='Times 16 bold').place(x=150,y=400)
+    a=Label(top,text="🤗🤗🤗🤗🤗",font='Arial 40 bold',bg="pink").place(x=200,y=500)
+
+def emoji8():
+    top=tkinter.Tk()
+    top.geometry('800x600+0+0')
+    top.title('Emoji bomb')
+    top.config(bg='green')
+    top.resizable(False,False)
+    r=Label(top,text="😏",bg='lightblue',font='Arial 120 bold').place(x=260,y=50)
+    u=Label(top,text="The FLIRT emoji",font='Arial 20 bold').place(x=240,y=250)
+    k=Label(top,text="I don't know why but Imma just be making this face 24/7",font='Times 16 bold').place(x=150,y=400)
+    a=Label(top,text="😏😏😏😏😏",font='Arial 40 bold',bg="lightblue").place(x=200,y=500)
+
+def emoji9():
+    top=tkinter.Tk()
+    top.geometry('800x600+0+0')
+    top.title('Emoji bomb')
+    top.config(bg='green')
+    top.resizable(False,False)
+    r=Label(top,text="😥",bg='yellow',font='Arial 120 bold').place(x=260,y=50)
+    u=Label(top,text="The DRAINED emoji",font='Arial 20 bold').place(x=230,y=250)
+    k=Label(top,text="No cap ... I'm drained by all this stuff. When exactly does it get better?",font='Times 16 bold').place(x=120,y=400)
+    a=Label(top,text="😥😥😥😥😥",font='Arial 40 bold',bg="yellow").place(x=200,y=500)
+
+    
+L1=Label(top,text="WELCOME",font="Arial 30 bold").place(x=20,y=50)
+L2=Label(top,text="TO",font="Arial 30 bold").place(x=270,y=120)
+L3=Label(top,text="EMOJI",font="Arial 30 bold").place(x=400,y=200)
+L4=Label(top,text="BOMB",font="Arial 30 bold").place(x=600,y=300)
+B1=Button(top,text="😁",font="Arial 30 bold",command=emoji1,bg='gold').place(x=0,y=480)
+B2=Button(top,text="🤣",font="Arial 30 bold",command=emoji2,bg='lightgreen').place(x=90,y=480)
+B3=Button(top,text="🥰",font="Arial 30 bold",command=emoji3,bg='red').place(x=180,y=480)
+B4=Button(top,text="😎",font="Arial 30 bold",command=emoji4,bg='gray').place(x=270,y=480)
+B5=Button(top,text="😊",font="Arial 30 bold",command=emoji5,bg='orange').place(x=360,y=480)
+B6=Button(top,text="🤩",font="Arial 30 bold",command=emoji6,bg='purple').place(x=450,y=480)
+B7=Button(top,text="🤗",font="Arial 30 bold",command=emoji7,bg='pink').place(x=540,y=480)
+B8=Button(top,text="😏",font="Arial 30 bold",command=emoji8,bg='lightblue').place(x=630,y=480)
+B9=Button(top,text="😥",font="Arial 30 bold",command=emoji9,bg='yellow').place(x=720,y=480)
+L5=Label(top,text="Press your emoji of choice to explore... ",bg="green").place(x=40,y=450)
+         
+top.mainloop()
